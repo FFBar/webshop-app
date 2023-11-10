fa62a68b87c4c98468654b2034866da2e999f37e
FFBar <gizey.fp@gmail.com>
2023-11-10 22:53:32 +0100

    feat: add stripe-module, implement onCheckout()

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from '../../components/models/CartItem.model';
 import { CartService } from '../../services/cart.service';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-cart',
@@ -20,7 +22,10 @@ export class CartComponent implements OnInit {
     'action',
   ];
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.cartService.cart.subscribe(_cart => {
@@ -37,19 +42,34 @@ export class CartComponent implements OnInit {
     return this.cartService.getTotal(item);
   }
 
-  onClearCart() {
+  onClearCart(): void {
     this.cartService.clearCart();
   }
 
-  onRemoveFromCart(element: CartItem) {
+  onRemoveFromCart(element: CartItem): void {
     this.cartService.removeFromCart(element);
   }
 
-  onAddQuantity(element: CartItem) {
+  onAddQuantity(element: CartItem): void {
     this.cartService.addQuantity(element);
   }
 
-  onSubtractQuantity(element: CartItem) {
+  onSubtractQuantity(element: CartItem): void {
     this.cartService.subtractQuantity(element);
+  }
+
+  onCheckout(): void {
+    this.http
+      .post('http://localhost:4242/checkout', {
+        items: this.cart,
+      })
+      .subscribe(async (res: any) => {
+        const stripe = await loadStripe(
+          'pk_test_51OB1H1H2MS9dC3lUalgv81gk83dikQtm1dVXXioHjEo5mAg54nmkZk1kgV0WdrTQ3xAuHNtCTts5XfoufJwPYdAf00rvnQn3YC'
+        );
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
   }
 }
